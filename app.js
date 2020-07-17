6fecac5f161361447cb6c846cf125a3f3d4daedf
Mr.Robot <classic.graal095@gmail.com>
2020-07-17 09:26:13 +0000

    exo save

---
+++ app.js
@@ -0,0 +1,16 @@
+var number=[1,15,25,20,50,23,54,1000]
+for(var i=0;i<number.length;i++){
+    if(number[i]%3==0 && number[i]%5==0 && number[i]>0 && number[i]<1001){
+        alert('SalutMec')
+    }else if(number[i]%3==0 && number%5!==0 && number>0 && number<1001){
+        alert('Salut')
+    }else if(number[i]%5==0 && number[i]%3!==0 && number[i]>0 && number[i]<1001){
+        alert('Mec')
+    }else if(number[i]<1){
+        alert('nombre trop petit')
+    }else if(number[i]>1000){
+        alert('nombre trop grand')
+    }else{
+        alert('Au revoir')
+    }
+}
